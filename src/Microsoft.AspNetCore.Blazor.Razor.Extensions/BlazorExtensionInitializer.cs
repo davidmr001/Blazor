b950ef05f7eaa9a95e21ccfae967ed335eb3f721
Ryan Nowak <nowakra@gmail.com>
2018-02-19 16:16:44 -0800

    Working blazor codegen

---
+++ src/Microsoft.AspNetCore.Blazor.Razor.Extensions/BlazorExtensionInitializer.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+#if RAZOR_2_1
+
+using System;
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.AspNetCore.Razor.Language.Extensions;
+
+namespace Microsoft.AspNetCore.Blazor.Razor
+{
+    public class BlazorExtensionInitializer : RazorExtensionInitializer
+    {
+        public override void Initialize(RazorProjectEngineBuilder builder)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            FunctionsDirective.Register(builder);
+            InheritsDirective.Register(builder);
+            TemporaryLayoutPass.Register(builder);
+            TemporaryImplementsPass.Register(builder);
+
+            builder.Features.Add(new ComponentDocumentClassifierPass());
+        }
+    }
+}
+
+#endif
